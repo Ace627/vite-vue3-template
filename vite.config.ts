57d39484a91047a4956b89899beee408e398793f
Legendary <1207588603@qq.com>
2023-05-01 14:02:28 +0800

    打包后自动移除 console 与 debugger

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,32 @@ export default defineConfig({
   ],
 
   server: {
-    host: true,
+    host: true, // 设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址
+    port: 5173, // 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口
+    strictPort: false, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+    open: false, // 是否在开发服务器启动时，自动在浏览器中打开应用程序
   },
 
   resolve: {
     alias: {
       '@': resolve(process.cwd(), 'src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+
+  build: {
+    outDir: 'dist', // 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整
+    assetsDir: 'assets', // 指定生成静态资源的存放目录。默认值为 assets ，可根据需要进行调整
+    assetsInlineLimit: 4096, // 图片转 base64 编码的阈值。为防止过多的 http 请求，Vite 会将小于此阈值的图片转为 base64 格式
+    chunkSizeWarningLimit: 500, // 规定触发警告的 chunk 大小。（以 kbs 为单位）
+    cssCodeSplit: true, // 启用/禁用 CSS 代码拆分
+    sourcemap: false, // 构建后是否生成 source map 文件
+    copyPublicDir: true, // 是否在构建阶段将 publicDir 目录中的所有文件复制到 outDir 目录中
+    minify: 'terser', // 指定使用哪种混淆器。默认为 esbuild，它比 terser 快 20-40 倍，压缩率只差 1%-2%
+    terserOptions: {
+      compress: {
+        drop_console: true, // 正式环境移除 console
+        drop_debugger: true, // 正式环境移除 debugger
+      },
     },
   },
 })
