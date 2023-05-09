a5c38e8feb990f4dcd2cb6ceaf9447e6d5dc8a99
legendary <1207588603@qq.com>
2023-05-09 14:29:25 +0800

    新增全局布局组件

---
+++ src/layout/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app-container">
+    <div id="star-five"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.app-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+#star-five {
+  cursor: pointer;
+  width: 600px;
+  height: 600px;
+  clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+  animation: run 6s infinite linear;
+}
+
+@keyframes run {
+  0% {
+    background-color: red;
+    transform: rotate(0deg);
+  }
+  10% {
+    background-color: orangered;
+  }
+  20% {
+    background-color: yellow;
+  }
+  30% {
+    background-color: green;
+  }
+  40% {
+    background-color: cyan;
+  }
+  50% {
+    background-color: blue;
+  }
+  60% {
+    background-color: purple;
+  }
+  100% {
+    background-color: red;
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,16 @@
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = []
+/* Base Layout */
+import Layout from '@/layout/index.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Layout',
+    component: Layout,
+  },
+]
 
 export default createRouter({
   // history: createWebHistory(), // 路径中无 # 号，但需要服务器提供支持
