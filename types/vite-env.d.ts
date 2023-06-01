ad5546c9d0e480c83e86e56d85b681dc635c25e0
legendary <1207588603@qq.com>
2023-06-01 14:39:25 +0800

    放开打包文件的输出目录的配置到环境变量

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -4,14 +4,11 @@
  */
 interface ImportMetaEnv {
   VITE_APP_TITLE: string // 应用标题
-
   VITE_BASE_URL: string // 接口地址
-
   VITE_BASE_API: string // 后端接口公共路径
-
   VITE_ROUTER_MODE: string // VueRouter 的路由模式 hash | history
-
   VITE_DROP_CONSOLE: string // 是否移除打包后的 conole.log 日志打印
+  VITE_OUT_DIR: string // 指定打包文件的输出目录
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const pathResolve = (path: string): string => resolve(process.cwd(), path)
 export default defineConfig(({ command, mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀
-  const { VITE_BASE_API, VITE_DROP_CONSOLE } = loadEnv(mode, process.cwd(), '')
+  const { VITE_BASE_API, VITE_DROP_CONSOLE, VITE_OUT_DIR } = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [
@@ -61,7 +61,7 @@ export default defineConfig(({ command, mode }) => {
     },
 
     build: {
-      outDir: 'dist', // 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整
+      outDir: VITE_OUT_DIR, // 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整
       assetsDir: 'assets', // 指定生成静态资源的存放目录。默认值为 assets ，可根据需要进行调整
       assetsInlineLimit: 4096, // 图片转 base64 编码的阈值。为防止过多的 http 请求，Vite 会将小于此阈值的图片转为 base64 格式
       chunkSizeWarningLimit: 500, // 规定触发警告的 chunk 大小。（以 kbs 为单位）
