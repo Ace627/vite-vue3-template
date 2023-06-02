5b9ca15222879b1015900e3d863eb8893e8a05a7
legendary <1207588603@qq.com>
2023-06-02 15:41:18 +0800

    添加二维码生成测试

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="app-container">
-    <h2>Unix时间戳(毫秒)：{{ timestampBy13 }}</h2>
-    <h2>Unix时间戳(秒)：{{ timestampBy10 }}</h2>
-    <h2>当前时间解析：{{ time }}</h2>
+    <h2>二维码生成测试</h2>
+    <Qrcode text="https://cn.vitejs.dev/guide/" logo-src="https://cn.vitejs.dev/logo.svg" />
+    <Qrcode text="https://cn.vitejs.dev/guide/" logo-src="https://cn.vitejs.dev/logo.svg" />
+    <Qrcode text="https://cn.vitejs.dev/guide/" />
+    <Qrcode text="https://cn.vitejs.dev/guide/" logo-src="https://cn.vitejs.dev/logo.svg" />
+    <Qrcode text="https://cn.vitejs.dev/guide/" logo-src="https://cn.vitejs.dev/logo.svg" />
+    <hr />
   </div>
 </template>
 
-<script setup lang="ts">
-import dayjs from 'dayjs'
-
-const timestampBy13 = ref(dayjs().valueOf())
-const timestampBy10 = ref(dayjs().unix())
-const time = ref(dayjs().format('YYYY-MM-DD HH:mm:ss'))
-
-setInterval(() => {
-  timestampBy13.value = dayjs().valueOf()
-  timestampBy10.value = dayjs().unix()
-  time.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
-}, 1000)
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 .app-container {
   position: relative;
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  // overflow: hidden;
 }
 
-h2 {
-  text-align: center;
-  padding: 16px 0;
+img {
+  margin: 6px;
+  border-radius: 16px;
+  box-shadow: 0 5px 10px 1px #0f15331a, inset -1px 1px 1px #fffc;
 }
 </style>
