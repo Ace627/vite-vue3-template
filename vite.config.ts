f10fb523594c5f3d25b51adbc0dbd5eeaae94e33
legendary <1207588603@qq.com>
2023-06-12 10:50:32 +0800

    打包后移除注释

--- vite.config.ts
+++ vite.config.ts
@@ -41,16 +41,24 @@ export default defineConfig(({ command, mode }) => {
     ],
 
     server: {
-      host: true, // 设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址
-      port: 5173, // 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口
-      strictPort: false, // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
-      open: false, // 是否在开发服务器启动时，自动在浏览器中打开应用程序
+      /** 设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址 */
+      host: true,
+      /** 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口 */
+      port: 5173,
+      /** 端口被占用时，是否直接退出 | 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口 */
+      strictPort: false,
+      /** 是否自动打开浏览器 */
+      open: false,
+      /** 是否允许跨域 */
+      cors: true,
     },
 
     resolve: {
       alias: {
-        '@': pathResolve('src'), // 设置 `@` 指向 `src` 目录
-        '#': pathResolve('types'), // 设置 `#` 指向 `types` 目录
+        /** 设置 `@` 指向 `src` 目录 */
+        '@': pathResolve('src'),
+        /** 设置 `#` 指向 `types` 目录 */
+        '#': pathResolve('types'),
       },
     },
 
@@ -64,18 +72,31 @@ export default defineConfig(({ command, mode }) => {
     },
 
     build: {
-      outDir: ENV_CONFIG.VITE_OUTPUT_DIR, // 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整
-      assetsDir: 'assets', // 指定生成静态资源的存放目录。默认值为 assets ，可根据需要进行调整
-      assetsInlineLimit: 4096, // 图片转 base64 编码的阈值。为防止过多的 http 请求，Vite 会将小于此阈值的图片转为 base64 格式
-      chunkSizeWarningLimit: 500, // 规定触发警告的 chunk 大小。（以 kbs 为单位）
-      cssCodeSplit: true, // 启用/禁用 CSS 代码拆分
-      sourcemap: false, // 构建后是否生成 source map 文件
-      copyPublicDir: true, // 是否在构建阶段将 publicDir 目录中的所有文件复制到 outDir 目录中
-      minify: 'terser', // 指定使用哪种混淆器。默认为 esbuild，它比 terser 快 20-40 倍，压缩率只差 1%-2%
+      /** 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整 */
+      outDir: ENV_CONFIG.VITE_OUTPUT_DIR,
+      /** 指定生成静态资源的存放目录。默认值为 assets ，可根据需要进行调整 */
+      assetsDir: 'assets',
+      /** 图片转 base64 编码的阈值。为防止过多的 http 请求，Vite 会将小于此阈值的图片转为 base64 格式 */
+      assetsInlineLimit: 4096,
+      /** 规定触发警告的 chunk 大小。（以 kbs 为单位） */
+      chunkSizeWarningLimit: 500,
+      /** 启用/禁用 CSS 代码拆分 */
+      cssCodeSplit: true,
+      /** 构建后是否生成 source map 文件 */
+      sourcemap: false,
+      /** 是否在构建阶段将 publicDir 目录中的所有文件复制到 outDir 目录中 */
+      copyPublicDir: true,
+      /** 指定使用哪种混淆器。默认为 esbuild，它比 terser 快 20-40 倍，压缩率只差 1%-2% */
+      /** Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效 */
+      minify: 'terser',
+      /** 在打包代码时移除 console.log、debugger 和 注释 */
       terserOptions: {
         compress: {
-          drop_console: ENV_CONFIG.VITE_DROP_CONSOLE === '1', // 正式环境移除 console
-          drop_debugger: true, // 正式环境移除 debugger
+          drop_console: ENV_CONFIG.VITE_DROP_CONSOLE === '1',
+          drop_debugger: true,
+        },
+        format: {
+          comments: false,
         },
       },
     },
