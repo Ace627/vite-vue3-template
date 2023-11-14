20ffc89fc2b8e037dd09ce1c6eddd2c2e26bfe43
当时只道是寻常 <1207588603@qq.com>
2023-11-14 16:19:08 +0800

    feat：方法库新增几个公用方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,24 @@
-/**
- * 深拷贝，支持日期、正则、函数
- */
+/** 生成指定范围的随机数 包含边界值 */
+export function randomCount(min = 2, max = 10): number {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+/** 延时函数 指定 ms 后再执行后续函数 */
+export function sleep(delay = 1000): Promise<void> {
+  return new Promise((resolve, _) => setTimeout(() => resolve(), delay))
+}
+
+/** 用 JS 获取全局 CSS 变量 */
+export function getCssVariableValue(cssVariableName: string): string {
+  return getComputedStyle(document.documentElement).getPropertyValue(cssVariableName)
+}
+
+/** 用 JS 设置全局 CSS 变量 */
+export function setCssVariableValue(cssVariableName: string, cssVariableValue: string): void {
+  document.documentElement.style.setProperty(cssVariableName, cssVariableValue)
+}
+
+/** 深拷贝，支持日期、正则、函数 */
 export function deepClone(source: any): any {
   if (!source || typeof source !== 'object') return source
   if (source instanceof Date) return new Date(source)
@@ -10,33 +28,24 @@ export function deepClone(source: any): any {
   return target
 }
 
-/**
- * 获取变量的真实类型
- */
+/** 获取变量的真实类型 */
 export function getRawType(variable: any): string {
   return Object.prototype.toString.call(variable).split(' ')[1].replace(']', '').toLowerCase()
 }
 
-/**
- * 反转字符串
- */
+/** 反转字符串 */
 export function reverseString(str: string): string {
   return str.split('').reverse().join('')
 }
 
-/**
- * 通过名称获取 URL 查询参数
- */
+/** 通过名称获取 URL 查询参数 */
 export function getQueryByName(name: string) {
   const queryNameRegExp = new RegExp(`[?&]${name}=([^&]*)(?:&|$)`)
   const queryNameMatch = window.location.href.match(queryNameRegExp)
   return queryNameMatch ? decodeURIComponent(queryNameMatch[1]) : ''
 }
 
-/**
- * @description 复制文本到剪贴板
- * @param {string} text 被复制的文本
- */
+/** 复制文本到剪贴板 */
 export function copyText(text: string): void {
   // 是否支持 navigator.clipboard 属性
   const isClipboardApiSupported = window.navigator && window.navigator.clipboard
