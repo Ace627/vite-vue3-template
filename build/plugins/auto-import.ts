12d17291dc944c00e2ced354253f2cf8508f7cc1
荒 <1207588603@qq.com>
2023-11-24 11:13:15 +0800

    chore：修改自动导入声明配置

--- build/plugins/auto-import.ts
+++ build/plugins/auto-import.ts
@@ -4,7 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 export const registerAutoImport = (): PluginOption => {
   return AutoImport({
     imports: ['vue', 'pinia', 'vue-router'], // 自动导入 vue、vue-router、Pinia 相关函数
-    dts: 'types/auto-import.d.ts',
+    dts: 'types/auto-generate/auto-import.d.ts',
     dirs: ['src/store/modules', 'src/hooks'], // 配置其它需要导入的文件目录
   })
 }
