ea0d822e1ee6377714bd3fd93482195982fd3f5a
Legendary <1207588603@qq.com>
2023-12-22 21:38:23 +0800

    feat：角度、弧度互转方法

---
+++ src/utils/math-helper.ts
@@ -0,0 +1,9 @@
+/** 角度转弧度 */
+export function angleToAngle(angle: number): number {
+  return (Math.PI / 180) * angle
+}
+
+/** 弧度转角度 */
+export function radiusToAngle(radius: number): number {
+  return 180 * Math.PI * radius
+}
