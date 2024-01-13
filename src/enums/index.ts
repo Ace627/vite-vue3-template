55190f313e886205ad4d6e499b50588ab3935e70
Legendary <1207588603@qq.com>
2024-01-13 14:32:36 +0800

    feat：性别枚举值

---
+++ src/enums/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 性别枚举值 标准号：GB/T 2261.1-2003
+ * 参考：https://openstd.samr.gov.cn/bzgk/gb/newGbInfo?hcno=0FC942D542BC6EE3C707B2647EF81CD8
+ */
+export enum GenderEnum {
+  /** 未知的性别 */
+  UNKNOWN = 0,
+  /** 男性 */
+  MALE = 1,
+  /** 女性 */
+  FEMALE = 2,
+}
