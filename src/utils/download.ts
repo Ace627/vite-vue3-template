7d0e92741ae7eab9d53fe6f9e0ac85a3c425d96e
荒 <1207588603@qq.com>
2024-01-16 12:21:00 +0800

    feat：图片转为 Base64 字符串

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -12,7 +12,7 @@ export function linkDownload(fileURL: string, fileName = Date.now().toString()):
   a.download = fileName
   document.body.appendChild(a)
   a.click()
-  a.remove()
+  document.body.removeChild(a)
 }
 
 /**
@@ -24,4 +24,30 @@ export function linkDownload(fileURL: string, fileName = Date.now().toString()):
 export function bufferToBlob(buffer: Buffer, blobType = 'application/actet-stream'): Blob {
   const unit8Array = new Uint8Array(buffer)
   return new Blob([unit8Array], { type: blobType })
+}
+
+/**
+ * 图片转为 Base64 字符串
+ * @param {String} imgURL 待转换的图片路径
+ * @param {String} type 转换后的图片类型
+ * @param  {Number} quality 转换后的图片质量
+ * @returns {String} Base64 字符串
+ */
+export function imageToBase64(imgURL: string, mimeType = 'image/jpeg', quality = 1): Promise<string> {
+  const img = new Image()
+  // 因为是网络资源所以会有图片跨域问题产生，此属性可以解决跨域问题
+  img.setAttribute('crossOrigin', 'anonymous')
+  // 如果需要兼容 iOS，这两个顺序一定不能换，先设置 crossOrigin 后设置 src
+  img.src = imgURL
+  return new Promise((resolve, reject) => {
+    img.onload = () => {
+      const cvs = document.createElement('canvas')
+      cvs.width = img.width
+      cvs.height = img.height
+      const ctx = cvs.getContext('2d')!
+      ctx.drawImage(img, 0, 0, cvs.width, cvs.height)
+      resolve(cvs.toDataURL(mimeType, quality))
+    }
+    img.onerror = (error: any) => reject(error)
+  })
 }
