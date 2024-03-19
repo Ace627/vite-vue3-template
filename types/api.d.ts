e55eb9cb98df9cbc2051a61bc07f0a334928d1cb
Legendary <1207588603@qq.com>
2024-03-19 19:08:16 +0800

    feat：列表类型的响应数据类型

--- types/api.d.ts
+++ types/api.d.ts
@@ -1,9 +1,27 @@
 /** 所有 api 接口的响应数据都应该遵守该格式 */
 interface ApiResponseData<T = any> {
   readonly code: number
-  readonly msg: string
-  data: Readonly<T>
+  readonly timestamp: number
+  readonly success: boolean
+  readonly message: string
+  result: Readonly<T>
+}
+
+/** 列表类型的响应数据类型 */
+interface TableResponseData<T = any> extends ApiResponseData {
+  result: {
+    total: number
+    list: T
+  }
+}
+
+/** 基础的分页查询参数 */
+interface QueryTableParams {
+  pageNo: number
+  pageSize: number
+  [key: string]: any
 }
 
 /* 定义类型别名，简化接口返回类型的书写 */
 type ApiResult<T = any> = Promise<ApiResponseData<T>>
+type TableResult<T = any> = Promise<TableResponseData<T>>
--- types/global.d.ts
+++ types/global.d.ts
@@ -3,3 +3,6 @@ type Recordable<T = any> = Record<string, T>
 
 /** 任意类型的 Function */
 type AnyFunction = (...args: any[]) => any
+
+/** 设备类型 */
+type DeviceType = 'mobile' | 'desktop'
