df4b78ea252ebb4320e86ce4fe680985ea4fc63d
荒 <1207588603@qq.com>
2024-04-18 14:06:37 +0800

    perf：useResize 注释

--- src/hooks/useResize.ts
+++ src/hooks/useResize.ts
@@ -1,17 +1,19 @@
-/** 参考 Bootstrap 的响应式设计 WIDTH = 992 */
-const WIDTH = 992
+/** 参考 Bootstrap 的响应式设计将最大移动端宽度设置为 992 */
+const MAX_MOBILE_WIDTH = 992
 
-/** 根据大小变化重新布局 */
+/** 根据浏览器宽度变化，变换 Layout 布局 */
 export default () => {
   const route = useRoute()
   const appStore = useApp()
 
-  const _isMobile = () => {
+  /** 用于判断当前设备是否为移动端 */
+  function _isMobile(): boolean {
     const rect = document.body.getBoundingClientRect()
-    return rect.width - 1 < WIDTH
+    return rect.width - 1 < MAX_MOBILE_WIDTH
   }
 
-  const _resizeHandler = () => {
+  /** 用于处理窗口大小变化事件 */
+  function _resizeHandler() {
     if (!document.hidden) {
       const isMobile = _isMobile()
       appStore.device = isMobile ? 'mobile' : 'desktop'
@@ -24,6 +26,7 @@ export default () => {
     }
   }
 
+  /** 监听路由变化，根据设备类型调整布局 */
   watch(
     () => route.name,
     () => {
@@ -33,10 +36,10 @@ export default () => {
     },
   )
 
-  onBeforeMount(() => {
-    window.addEventListener('resize', _resizeHandler)
-  })
+  /** 在组件挂载前添加窗口大小变化事件监听器 */
+  onBeforeMount(() => window.addEventListener('resize', _resizeHandler))
 
+  /** 在组件挂载后根据窗口大小判断设备类型并调整布局 */
   onMounted(() => {
     if (_isMobile()) {
       appStore.device = 'mobile'
@@ -44,7 +47,6 @@ export default () => {
     }
   })
 
-  onBeforeUnmount(() => {
-    window.removeEventListener('resize', _resizeHandler)
-  })
+  /** 在组件卸载前移除窗口大小变化事件监听器 */
+  onBeforeUnmount(() => window.removeEventListener('resize', _resizeHandler))
 }
--- src/views/ExceptionPage/NotFound.vue
+++ src/views/ExceptionPage/NotFound.vue
@@ -1,14 +1,14 @@
 <template>
-  <div :class="classes">
-    <div class="exception-content flex justify-center items-center">
+  <div :class="classes" class="exception-container clearFix">
+    <div class="exception-content flex-center w-full h-70% mt-10vh overflow-hidden">
       <div class="img-container">
         <img src="https://gw.alipayobjects.com/zos/rmsportal/KpnpchXsobRgLElEozzI.svg" draggable="false" />
       </div>
 
       <div class="content">
-        <h1 class="text-bold">404</h1>
+        <h1 class="font-bold">404</h1>
         <div class="desc">抱歉，你访问的页面不存在或无权访问</div>
-        <router-link to="/">返回首页</router-link>
+        <router-link to="/" class="c-#1890ff">返回首页</router-link>
       </div>
     </div>
   </div>
@@ -21,16 +21,11 @@ defineOptions({ name: 'NotFound' })
 useResize()
 /** 读取 Pinia 仓库 */
 const appStore = useApp()
-const classes = computed(() => ['exception-container', 'clearFix', appStore.device])
+const classes = computed(() => [appStore.device])
 </script>
 
 <style lang="scss" scoped>
 .exception-content {
-  width: 100%;
-  height: 70%;
-  margin-top: 10vh;
-  overflow: hidden;
-
   .img-container {
     padding-right: 52px;
     img {
@@ -42,16 +37,12 @@ const classes = computed(() => ['exception-container', 'clearFix', appStore.devi
   .content {
     h1 {
       color: #434e59;
-      margin: 16px auto;
       font-size: 72px;
     }
     .desc {
-      margin: 16px auto;
+      margin-bottom: 16px;
       color: rgba(0, 0, 0, 0.45);
       font-size: 20px;
-    }
-    a {
-      color: #1890ff;
     }
   }
 }
