f006de5ac62a7b2857533081e86b905a90259e20
荒 <1207588603@qq.com>
2024-05-06 17:25:37 +0800

    feat：封装 IconFont 全局组件，简化字体图标使用

---
+++ src/components/IconFont/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <i :class="classes" :style="styles" v-bind="$attrs"></i>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'IconFont' })
+import type { CSSProperties } from 'vue'
+
+const props = defineProps({
+  /** 图标名字 */
+  name: { type: String, required: true },
+  /** 图标大小 */
+  size: { type: [Number, String], default: '1em' },
+  /** 图标颜色 */
+  color: { type: String, default: 'inherit' },
+  /** 图标是否加粗 */
+  bold: { type: Boolean, default: false },
+})
+
+const styles = computed<CSSProperties>(() => {
+  const { size, color, bold } = props
+  const fontSize = typeof size === 'string' ? size : `${size}px`
+  const fontWeight = bold ? 'bold' : 'normal'
+  return { fontSize, color, fontWeight }
+})
+const classes = computed(() => {
+  return `iconfont icon-${props.name} cursor-pointer`
+})
+</script>
+
+<style lang="scss" scoped></style>
