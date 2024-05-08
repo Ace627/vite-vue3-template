96ad212f1f1e78590e77461b38cd59ed4767125b
荒 <1207588603@qq.com>
2024-05-08 14:09:15 +0800

    perf：统一校验方法的声明

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -5,7 +5,7 @@ export function isPhone(phone: string): boolean {
 }
 
 /** 判断版本号格式是否为 X.Y.Z */
-export const isVersion = (version: string) => {
+export function isVersion(version: string): boolean {
   const reg = /^\d+(?:\.\d+){2}$/
   return reg.test(version)
 }
@@ -45,55 +45,55 @@ export function isChinese(str: string): boolean {
 }
 
 /** 判断时间格式是否为 24 小时制（HH:mm:ss） */
-export function is24H(time: string) {
+export function is24H(time: string): boolean {
   const reg = /^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/
   return reg.test(time)
 }
 
 /** 校验是否为外链 */
-export function isExternal(path: string) {
+export function isExternal(path: string): boolean {
   const reg = /^(https?:|mailto:|tel:)/
   return reg.test(path)
 }
 
 /** 判断是否为网址（带协议） */
-export const isUrl = (url: string) => {
+export function isUrl(url: string): boolean {
   const reg = /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/
   return reg.test(url)
 }
 
 /** 判断是否为网址或 IP（带端口） */
-export const isUrlPort = (url: string) => {
+export function isUrlPort(url: string): boolean {
   const reg = /^((ht|f)tps?:\/\/)?[\w-]+(\.[\w-]+)+:\d{1,5}\/?$/
   return reg.test(url)
 }
 
 /** 判断是否为域名（不带协议） */
-export const isDomain = (domain: string) => {
+export function isDomain(domain: string): boolean {
   const reg = /^([0-9a-zA-Z-]{1,}\.)+([a-zA-Z]{2,})$/
   return reg.test(domain)
 }
 
 /** 检验是否为 IPv4 地址 */
-export function isIPv4(ip: string) {
+export function isIPv4(ip: string): boolean {
   const reg = /^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$/
   return reg.test(ip)
 }
 
 /** 判断是否为 MAC 地址 */
-export function isMac(mac: string) {
+export function isMac(mac: string): boolean {
   const reg = /^(([a-f0-9][0,2,4,6,8,a,c,e]:([a-f0-9]{2}:){4})|([a-f0-9][0,2,4,6,8,a,c,e]-([a-f0-9]{2}-){4}))[a-f0-9]{2}$/i
   return reg.test(mac)
 }
 
 /** 判断是否为车牌（兼容新能源车牌） */
-export function isLicensePlate(plate: string) {
+export function isLicensePlate(plate: string): boolean {
   const reg = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/
   return reg.test(plate)
 }
 
 /** 判断是否为第二代身份证（18 位） */
-export function isChineseIdCard(content: string) {
+export function isChineseIdCard(content: string): boolean {
   const reg = /^[1-9]\d{5}(?:18|19|20)\d{2}(?:0[1-9]|10|11|12)(?:0[1-9]|[1-2]\d|30|31)\d{3}[\dXx]$/
   return reg.test(content)
 }
