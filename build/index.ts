a8b5db4b6e10d5b32d046d8dbc6bfe039f85cfa2
荒 <1207588603@qq.com>
2024-05-09 18:28:38 +0800

    perf：SvgIcon 图标名称类型的声明逻辑

--- build/index.ts
+++ build/index.ts
@@ -1,5 +1,5 @@
 import path from 'path' // path 模块提供了一些工具函数，用于处理文件与目录的路径
-import './scripts/auto-svg-type'
+import './scripts/generate-svg-type' // 项目启动时自动生成 SvgIcon 的类型
 
 /** 处理环境变量 */
 export function warpperEnv(env_config: Recordable<string>): ViteEnv {
---
+++ build/scripts/generate-svg-type.ts
@@ -0,0 +1,25 @@
+import fs from 'fs/promises'
+import { pathResolve } from '..'
+
+/** Svg Icon 的存放目录 */
+const svgIconDir = pathResolve('src/assets/svg-icons')
+/** Svg Icon 声明文件的存放位置 */
+const svgIconTypeFileURL = pathResolve('types/auto-generate/auto-svg-name.d.ts')
+
+/** 用来自动读取 svg 图标目录并用其名字生成一个 Svg 名字的枚举值；每次新增图标都需要重启项目来更新声明文件 */
+async function generateSvgIconType(dir: string, outputFile: string) {
+  try {
+    // 读取指定目录下的所有图标文件名 含后缀
+    const iconFullNameList = await fs.readdir(dir)
+    // 获取去除后缀后的文件名数组
+    const iconNameList = iconFullNameList.map((item) => item.replace('.svg', ''))
+    // 生成 SvgIcon 的 name 的枚举值字符串
+    const types = iconNameList.reduce((prev, item, index) => (prev += iconNameList.length - 1 === index ? `'${item}'` : `'${item}' | `), '')
+    // 写入声明文件 如果文件已存在则覆盖
+    await fs.writeFile(outputFile, `type SvgIconName = ${types}`, { flag: 'w' })
+  } catch (error) {
+    console.log('error: ', error)
+  }
+}
+
+generateSvgIconType(svgIconDir, svgIconTypeFileURL)
