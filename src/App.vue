4ada7f1fbe5dab3951339a93e288fc02df1edcb5
Legendary <1207588603@qq.com>
2024-05-12 22:08:10 +0800

    perf：项目初始化配置

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,6 @@
 
 <script setup lang="ts">
 defineOptions({ name: 'App' })
-
-/** 读取 Pinia 仓库 */
-const settingStore = useSetting()
-
-/** 监听色值变量自动切换色彩模式 */
-watchEffect(() => {
-  const classes = ['showGreyMode', 'showColorWeakness']
-  const rootClassList = document.documentElement.classList
-  classes.forEach((className) => rootClassList.contains(className) && rootClassList.remove(className))
-  if (settingStore.showGreyMode) rootClassList.add('showGreyMode')
-  if (settingStore.showColorWeakness) rootClassList.add('showColorWeakness')
-})
 </script>
 
 <style lang="scss" scoped></style>
