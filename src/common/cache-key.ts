22141c0f8ab13781a8253320e1719a610f3a4837
荒 <1207588603@qq.com>
2024-05-20 15:14:51 +0800

    perf：对 localStorage 和 sessionStorage 的二次封装

--- src/common/cache-key.ts
+++ src/common/cache-key.ts
@@ -1,9 +1,11 @@
+import pkg from '../../package.json'
+
 /** 缓存键的前缀 */
-const CACHE_PREFIX = `v3-app`
+const CACHE_PREFIX = pkg.name.toUpperCase()
 
 /** 缓存数据时用到的 Key */
 export class CacheKey {
-  static readonly ACCESS_TOKEN = `${CACHE_PREFIX}_ACCESS_TOKEN_KEY`
-  static readonly SIDEBAR_STATUS = `${CACHE_PREFIX}_SIDEBAR_STATUS_KEY`
-  static readonly PROJECT_CONFIG = `${CACHE_PREFIX}_PROJECT_CONFIG_KEY`
+  static readonly ACCESS_TOKEN = `${CACHE_PREFIX}-ACCESS-TOKEN`
+  static readonly SIDEBAR_STATUS = `${CACHE_PREFIX}-SIDEBAR-STATUS`
+  static readonly PROJECT_CONFIG = `${CACHE_PREFIX}-PROJECT-CONFIG`
 }
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,2 @@
-export { CacheKey } from './cache-key'
-export { WebStorage } from './web-storage'
+export { CacheKey } from './cache-key' // 对缓存的 Key 的统一处理
+export { WebStorage } from './web-storage' // 对 localStorage 和 sessionStorage 的二次封装
