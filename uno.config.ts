24168367c072d593fd3a830fb88bc7674a314578
荒 <1207588603@qq.com>
2024-05-30 16:04:32 +0800

    perf：UnoCSS 配置

--- uno.config.ts
+++ uno.config.ts
@@ -20,7 +20,14 @@ export default defineConfig({
   ],
 
   /** 规则定义工具类和生成的 CSS。UnoCSS 有许多内置规则，但也允许轻松添加自定义规则 https://unocss.nodejs.cn/config/rules */
-  rules: [['bg-repeat-none', { 'background-repeat': 'no-repeat' }]],
+  rules: [
+    [/wh-(.+)$/, ([, d]) => ({ width: `${d}`, height: `${d}` })],
+    [/mtb-(.+)$/, ([, d]) => ({ margin: `${d} 0` })],
+    [/mlr-(.+)$/, ([, d]) => ({ margin: `0 ${d}` })],
+    [/ptb-(.+)$/, ([, d]) => ({ padding: `${d} 0` })],
+    [/plr-(.+)$/, ([, d]) => ({ padding: `0 ${d}` })],
+    ['bg-repeat-none', { 'background-repeat': 'no-repeat' }],
+  ],
 
   /** 受 Windi CSS 的 启发，快捷方式可让你将多个规则组合成一个简写 https://unocss.nodejs.cn/config/shortcuts */
   shortcuts: {
@@ -28,7 +35,7 @@ export default defineConfig({
     'wh-screen': 'w-screen h-screen', // 宽高视口铺满
     'flex-center': 'flex justify-center items-center', // 利用 flex 使子元素垂直水平居中
     clearFix: 'before:content-empty before:table before:clear-both after:content-empty after:table after:clear-both', // 同时解决高度塌陷和外边距折叠的问题
-    'app-content': 'relative wh-full p-16px', // 业务页面几乎都应该在根元素上挂载 class="app-content"，以保持页面美观
+    'app-content': 'relative w-full p-16px', // 业务页面几乎都应该在根元素上挂载 class="app-content"，以保持页面美观
   },
 
   transformers: [
