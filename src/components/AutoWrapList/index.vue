b9fb4332a10cc4997d31fc3bd77390b9fd894c22
Legendary <1207588603@qq.com>
2024-06-21 23:18:29 +0800

    feat：等高列表自适应换行组件

---
+++ src/components/AutoWrapList/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="auto-wrap-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'AutoWrapList' })
+import { isString } from 'lodash-es'
+
+const props = defineProps({
+  /** 列表项间隔 */
+  gap: { type: [String, Number], default: 16 },
+  /** 列表项最小宽度 */
+  minWidth: { type: [String, Number], default: 375 },
+})
+
+const gap = computed(() => (isString(props.gap) ? props.gap : `${props.gap}px`))
+const minWidth = computed(() => (isString(props.minWidth) ? props.minWidth : `${props.minWidth}px`))
+const repeat = computed(() => `repeat(auto-fill, minmax(${minWidth.value}, 1fr))`)
+</script>
+
+<style lang="scss" scoped>
+.auto-wrap-list {
+  display: grid;
+  grid-template-columns: v-bind(repeat);
+  gap: v-bind(gap);
+}
+</style>
