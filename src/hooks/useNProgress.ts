d8e3c49f3bf00754e6cf8766d307e9260d058120
Legendary <1207588603@qq.com>
2024-06-23 21:49:19 +0800

    perf：部分 NProgress 参数的描述信息

--- src/hooks/useNProgress.ts
+++ src/hooks/useNProgress.ts
@@ -5,8 +5,24 @@ interface NProgressConfig extends NProgressOptions {
   show: boolean
 }
 
-export default (config: Partial<NProgressConfig> = { show: true }) => {
-  config.showSpinner = config.showSpinner || false // 右侧加载小圈圈默认不展示
+const defaultConfig: Partial<NProgressConfig> = {
+  /** CSS3 缓冲动画字符串 如 ease、linear、ease-in、ease-out、ease-in-out、cubic-bezier 等 */
+  easing: 'ease',
+  /** 指定进度条的父容器 */
+  parent: 'body',
+  /** 自定义配置 方便利用环境变量统一控制加载条的显示与否 */
+  show: true,
+  /** 是否显示右侧的环形进度动画 */
+  showSpinner: false,
+  /** 是否开启自动递增 */
+  trickle: true,
+  /** 设置开始时最低百分比 */
+  minimum: 0.08,
+  /** 动画速度 单位 ms */
+  speed: 200,
+}
+
+export default (config = defaultConfig) => {
   NProgress.configure(config)
 
   const start = () => config.show && NProgress.start()
