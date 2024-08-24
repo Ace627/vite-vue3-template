f410a8452c04528268de3953e6065f67a4377c70
Legendary <1207588603@qq.com>
2024-08-24 12:41:26 +0800

    修复：路由 meta 类型配置无效的异常

--- types/vue-router/vue-router.d.ts
+++ types/vue-router/vue-router.d.ts
@@ -1,10 +1,15 @@
 import 'vue-router'
 
-/** VueRouter 中 Meta 字段的类型声明 */
 declare module 'vue-router' {
   // RouteLocationNormalizedLoaded 是 route 的类型 对其部分属性的类型进行重写
   interface RouteLocationNormalizedLoaded {
     query: Partial<VueRouteQuery> // types/vue-router/route-query.d.ts
-    meta: Partial<VueRouteMeta> // types/vue-router/route-meta.d.ts
   }
+
+  /**
+   * 路由记录中的 meta 字段的类型接口
+   * 文件地址：types/vue-router/route-meta.d.ts
+   * 官网文献：https://router.vuejs.org/zh/guide/advanced/meta.html#TypeScript
+   */
+  interface RouteMeta extends Partial<VueRouteMeta> {}
 }
