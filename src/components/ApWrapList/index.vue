0a307ef3e8e71bc79583b1ee997a5f3df25531af
当时只道是寻常 <1207588603@qq.com>
2024-10-11 17:25:10 +0800

    perf: AutoWrapList --> ApWrapList

--- src/components/ApWrapList/index.vue
+++ src/components/ApWrapList/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="auto-wrap-list">
+  <div class="ap-wrap-list">
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
-defineOptions({ name: 'AutoWrapList' })
+defineOptions({ name: 'ApWrapList' })
 import { isString } from 'lodash-es'
 
 const props = defineProps({
   /** 列表项间隔 */
   gap: { type: [String, Number], default: 16 },
   /** 列表项最小宽度 */
-  minWidth: { type: [String, Number], default: 375 },
+  minWidth: { type: [String, Number], default: 375 }
 })
 
 const gap = computed(() => (isString(props.gap) ? props.gap : `${props.gap}px`))
@@ -21,7 +21,7 @@ const repeat = computed(() => `repeat(auto-fill, minmax(${minWidth.value}, 1fr))
 </script>
 
 <style lang="scss" scoped>
-.auto-wrap-list {
+.ap-wrap-list {
   display: grid;
   grid-template-columns: v-bind(repeat);
   gap: v-bind(gap);
--- types/global-components.d.ts
+++ types/global-components.d.ts
@@ -2,11 +2,11 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AutoWrapList: typeof import('../src/components/AutoWrapList/index.vue')['default']
+    ApTaiChi: typeof import('../src/components/ApTaiChi/index.vue')['default']
+    ApWrapList: typeof import('../src/components/ApWrapList/index.vue')['default']
     Editor: typeof import('../src/components/Editor/index.vue')['default']
     IconFont: typeof import('../src/components/IconFont/index.vue')['default']
     InnerLink: typeof import('../src/components/InnerLink/index.vue')['default']
     QRCode: typeof import('../src/components/QRCode/index.vue')['default']
-    ApTaiChi: typeof import('../src/components/ApTaiChi/index.vue')['default']
   }
 }
