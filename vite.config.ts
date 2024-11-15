d149f275f06d012b84e1d5d524d84b1203024b19
当时只道是寻常 <1207588603@qq.com>
2024-11-15 13:18:30 +0800

    fix: 代理路径重写异常的问题

--- vite.config.ts
+++ vite.config.ts
@@ -9,43 +9,43 @@ const root: string = process.cwd()
 export default defineConfig(({ command, mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀
-  const VITE_ENV = warpperEnv(loadEnv(mode, root, 'VITE_')) // 对原生环境变量进行二次处理
+  const runtimeConfig = warpperEnv(loadEnv(mode, root, 'VITE_')) // 对原生环境变量进行二次处理
   const isBuild = command === 'build' // 当前是否是生产模式
 
   return {
     // 部署应用包时的基本 URL
-    base: VITE_ENV.VITE_PUBLIC_PATH,
+    base: runtimeConfig.VITE_PUBLIC_PATH,
 
-    plugins: generateVitePlugins(VITE_ENV, isBuild),
+    plugins: generateVitePlugins(runtimeConfig, isBuild),
 
     resolve: {
       alias: [
         /** 设置 `@` 指向 `src` 目录 */
         { find: '@', replacement: pathResolve('src') },
         /** 设置 `#` 指向 `types` 目录 */
-        { find: '#', replacement: pathResolve('types') },
-      ],
+        { find: '#', replacement: pathResolve('types') }
+      ]
     },
 
     server: {
       /** 设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址 */
       host: true,
       /** 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口 */
-      port: VITE_ENV.VITE_SERVER_PORT,
+      port: runtimeConfig.VITE_SERVER_PORT,
       /** 端口被占用时，是否直接退出 | 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口 */
       strictPort: false,
       /** 是否自动打开浏览器 */
-      open: VITE_ENV.VITE_AUTO_OPEN,
+      open: runtimeConfig.VITE_AUTO_OPEN,
       /** 是否允许跨域 */
       cors: true,
       /** 反向代理配置（主要是开发时用来解决跨域问题） */
       proxy: {
-        [VITE_ENV.VITE_BASE_API]: {
-          target: VITE_ENV.VITE_BASE_URL,
+        [runtimeConfig.VITE_BASE_API]: {
+          target: runtimeConfig.VITE_BASE_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(VITE_ENV.VITE_BASE_API, ''),
-        },
-      },
+          rewrite: (path) => path.replace('/dev-api', '')
+        }
+      }
     },
 
     css: {
@@ -63,13 +63,13 @@ export default defineConfig(({ command, mode }) => {
       preprocessorOptions: {
         scss: {
           // additionalData: `@use "@/ styles/custom-element-plus/element-variables.scss" as *;`,
-        },
-      },
+        }
+      }
     },
 
     build: {
       /** 指定打包文件的输出目录。默认值为 dist ，当 dist 被占用或公司有统一命名规范时，可进行调整 */
-      outDir: VITE_ENV.VITE_OUTPUT_DIR,
+      outDir: runtimeConfig.VITE_OUTPUT_DIR,
       /** 图片转 base64 编码的阈值。为防止过多的 http 请求，Vite 会将小于此阈值的图片转为 base64 格式 */
       assetsInlineLimit: 4096,
       /** 规定触发警告的 chunk 大小。（以 kbs 为单位） */
@@ -124,9 +124,9 @@ export default defineConfig(({ command, mode }) => {
             // 这个 return 的值就是打包的名称
             // 可以利用浏览器的缓存机制 减少请求次数
             if (chunk.includes('node_modules')) return 'vendor'
-          },
-        },
-      },
+          }
+        }
+      }
     },
 
     esbuild: {
@@ -134,12 +134,12 @@ export default defineConfig(({ command, mode }) => {
        * 打包后是否移除所有的 console、debugger
        * http://cosarty.cn/doc/essay/handbook/esbuild.html#drop
        */
-      drop: VITE_ENV.VITE_DROP_CONSOLE ? ['console', 'debugger'] : [],
+      drop: runtimeConfig.VITE_DROP_CONSOLE ? ['console', 'debugger'] : [],
       /**
        * 打包后是否移除所有的注释
        * http://esbuild.docschina.org/api/#legal-comments
        */
-      legalComments: VITE_ENV.VITE_CLEAR_COMMENT ? 'none' : 'inline',
-    },
+      legalComments: runtimeConfig.VITE_CLEAR_COMMENT ? 'none' : 'inline'
+    }
   }
 })
