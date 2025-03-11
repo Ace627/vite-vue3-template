f2f7e6c475972df566ee3f2577b0d8e72831d02c
当时只道是寻常 <1207588603@qq.com>
2025-03-11 09:48:54 +0800

    feat：获取当前设备的屏幕 DPI 的方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -69,3 +69,19 @@ export function getQueryObject(url?: string): Record<string, string> {
   }
   return query
 }
+
+/**
+ * 获取当前设备的屏幕 DPI（每英寸像素数）
+ *  此函数通过创建一个宽度为 1 英寸的临时不可见 div 元素，
+ *  将其添加到文档中后读取其 offsetWidth 属性来计算当前设备的 DPI，然后移除该元素。
+ * @returns {number} 当前设备的 DPI（即 1 英寸所对应的像素数）
+ */
+export function getDeviceDpi(): number {
+  const tempDiv = document.createElement('div')
+  tempDiv.style.width = '1in'
+  tempDiv.style.visibility = 'hidden'
+  document.body.appendChild(tempDiv)
+  const dpi = tempDiv.offsetWidth
+  document.body.removeChild(tempDiv)
+  return dpi
+}
--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="app-content">首页</div>
+  <div class="app-content">
+    <div>{{ VITE_APP_TITLE }}</div>
+    <div>设备 DPI {{ getDeviceDpi() }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({ name: 'Dashboard' })
+import { getDeviceDpi } from '@/utils'
 
 // 全局变量获取
-const VITE_APP_TITLE = __RUNTIME_CONFIG__.VITE_APP_TITLE
-console.log('__RUNTIME_CONFIG__: ', __RUNTIME_CONFIG__)
-console.log('VITE_APP_TITLE: ', VITE_APP_TITLE) // Vue3 Common Template
+const VITE_APP_TITLE = computed(() => __RUNTIME_CONFIG__.VITE_APP_TITLE)
 </script>
 
 <style lang="scss" scoped></style>
